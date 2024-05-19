07750c8bafe1a7f9376b153cd988e2822303fad8
Mohammad Beltagy <166910901+im-beltagy@users.noreply.github.com>
2024-05-19 05:31:31 +0300

    case-studies cards links

--- src/js/Case-Studies/script.js
+++ src/js/Case-Studies/script.js
@@ -1,12 +1,12 @@
-const cards = Array.from(
-  document.querySelectorAll(".caseStudies__cards--card")
-).forEach((card) => {
-  card.addEventListener("click", () => {
-    window.location.href = card.querySelector(
-      ".caseStudies__cards--cardLink"
-    ).href;
-  });
-});
+// const cards = Array.from(
+//   document.querySelectorAll(".caseStudies__cards--card")
+// ).forEach((card) => {
+//   card.addEventListener("click", () => {
+//     window.location.href = card.querySelector(
+//       ".caseStudies__cards--cardLink"
+//     ).href;
+//   });
+// });
 
 const featuredCards = Array.from(
   document.querySelectorAll(".featuredInsights__card")
