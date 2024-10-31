3cad27808f8b85e718e965ec2bf6639ef9d8fd2c
im-beltagy <imbeltagy@gmail.com>
2024-10-31 21:01:25 +0300

    .

---
+++ src/js/swipers/NeonHero.js
@@ -0,0 +1,9 @@
+const neonHeroSwiper = new Swiper(".neonHero__swiper", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  loop: true,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+});
